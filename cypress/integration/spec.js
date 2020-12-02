cb36caebbeb6c6d9ae85dcbe8da8d6ce06b902ab
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-12-01 23:02:33 -0500

    fix the anchor links

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,7 +1,32 @@
 /// <reference types="cypress" />
 
-describe("Blog", () => {
+describe("Blog", { viewportWidth: 600, viewportHeight: 1200 }, () => {
   it("loads", () => {
     cy.visit("/");
+  });
+
+  it("has anchor tags", () => {
+    cy.visit("2020/develop-preview-test");
+    cy.contains("a", "#")
+      .scrollIntoView()
+      .should("not.have.attr", "href", "#undefined");
+  });
+
+  it("has anchor tags using cy.contains with message", () => {
+    cy.visit("2020/develop-preview-test");
+    cy.contains("a", "#")
+      .scrollIntoView()
+      .should($a => {
+        const message = $a.parent().parent().text();
+        expect($a, message).to.not.have.attr("href", "#undefined");
+      });
+  });
+
+  it("has anchor tags using cy.get and .each", () => {
+    cy.visit("2020/develop-preview-test");
+    cy.get("a:contains(#)").each($a => {
+      const message = $a.parent().parent().text();
+      expect($a, message).to.not.have.attr("href", "#undefined");
+    });
   });
 });
